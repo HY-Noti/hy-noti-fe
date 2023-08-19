0336a0495b8e0b1aec13c6c1fb9adf553cc965c9
8ark <sorlti6952@gmail.com>
2023-08-19 10:26:37 +0900

    init

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import '@/styles/globals.css'
+import { CssBaseline, GeistProvider } from '@geist-ui/core'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <GeistProvider>
+      <CssBaseline/>
+      <Component {...pageProps} />
+    </GeistProvider>
+  )
+}
